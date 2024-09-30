ed7d02bfa10e92384bc9fd2d44a7b6cffd1baeb9
Joshua Ordehi <45109738+ordehi@users.noreply.github.com>
2024-09-30 11:54:14 -0400

    add dynamic value scraping

--- scraping/dynamic.py
+++ scraping/dynamic.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
 url = "https://automated.pythonanywhere.com/"
 
 
 def get_driver(url):
     options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    options.add_argument("--log-level=3")
     options.add_argument("disable-infobars")
     options.add_argument("start-maximized")
     options.add_argument("disable-dev-shm-usage")
@@ -22,11 +26,20 @@ def get_driver(url):
     return driver
 
 
+def clean_text(text):
+    """Extract the temperature from text"""
+    output = float(text.split(": ")[1])
+    return output
+
+
 def main():
     driver = get_driver(url)
-    element = driver.find_element(by="xpath",
-                                  value='/html/body/div[1]/div/h1[2]')
-    return element.text
+    time.sleep(2)
+    # element = driver.find_element(by="xpath",
+    #                               value='/html/body/div[1]/div/h1[2]')
+    element = driver.find_element(By.CSS_SELECTOR,
+                                  value='#displaytimer')
+    return clean_text(element.text)
 
 
 print(main())
